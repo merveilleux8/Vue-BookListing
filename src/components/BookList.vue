79bc79a587036ef459dcf438da8ed08822512f25
Merve Pehlivan <merve.pehlivan@foriba.com>
2020-02-04 15:51:40 +0300

    Completed Module

---
+++ src/components/BookList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div></template
+>
+
+<script>
+export default {
+  name: "Booklist",
+  data() {
+    return {
+      title: "All Books"
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+</style>
